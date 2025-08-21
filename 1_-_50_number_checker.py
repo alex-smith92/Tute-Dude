bdccfc22fa6ea7c599dd289d58dd6f37a001c869
alex-smith92 <ALAKHKIDUNIYA@GMAIL.COM>
2025-08-21 10:48:08 +0530

    Add files via upload

---
+++ 1_-_50_number_checker.py
@@ -0,0 +1,9 @@
+# Initialize a variable to store the sum
+total_sum = 0
+
+# Use a for loop to iterate from 1 to 50
+for i in range(1, 51):
+    total_sum += i
+
+# Display the final sum
+print("The sum of integers from 1 to 50 is:", total_sum)
---
+++ Odd_checker.py
@@ -0,0 +1,8 @@
+# Step 1: Take input from the user
+num = int(input("Enter an integer: "))
+
+# Step 2: Check whether the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
